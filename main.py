1880618c48aaaf73a6aaa72c08bb51512123f424
Timmy <timilehinoluwaseyi07@gmail.com>
2025-07-14 21:44:05 +0100

    refactor: remove unused import of os module

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import logging
-import os
 from dotenv import load_dotenv
 
 # Import our custom modules
